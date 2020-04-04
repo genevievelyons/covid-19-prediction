49aad87094c3a882633497224b1d342901e0cb1d
genevievelyons <glyons@hsph.harvard.edu>
2020-04-04 13:08:19 -0400

    data update

--- load_covid_data.py
+++ load_covid_data.py
@@ -34,7 +34,7 @@ g = git.cmd.Git(git_dir)
 g.pull()
 
 ################
-## Load the data
+## Load the data - US Only
 ################
 
 #Load data
@@ -83,11 +83,60 @@ np.sum(data[(data.combined_key.str[0:10] != 'Unassigned') & (data.date > '3/22/2
 #data
 
 ################
-## Export Data
+## Export Data - US Only
 ################
 
 data.to_csv('covid-19-prediction/data/covid_data.csv', index = False)
 
-#Push to git
-#g.push()
+
+
+################
+## Load the data - Global
+################
+
+#Load data
+confirmed = pd.read_csv('COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv', dtype = str)  
+deaths = pd.read_csv('COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv', dtype = str)  
+
+#List of keys
+keys = confirmed.loc[:,"Province/State":"Long"]
+
+#Columns
+c = list(keys.columns)
+c.append('confirmed')
+c.append('deaths')
+c.append('date')
+
+data = pd.DataFrame(columns = c)
+
+#Pivot the columns
+for col in confirmed.columns[4:]:
+    df = pd.concat([keys,confirmed.loc[:,col]], axis = 1)
+    df.rename(columns = {col: 'confirmed'}, inplace = True)
+    df = pd.concat([df, deaths.loc[:,col]], axis = 1)
+    df.rename(columns = {col: 'deaths'}, inplace = True)
+    df["date"] = col
+    data = pd.concat([data, df], axis = 0, sort = False)
+
+#Names
+data.columns = map(str.lower, data.columns)
+data.rename(columns = {'province/state':'province_state', 'country/region':'country_region'}, inplace = True)
+
+#Data types
+data['date'] = pd.to_datetime(data['date'])
+data['lat'] = pd.to_numeric(data['lat'])
+data['long'] = pd.to_numeric(data['long'])
+data['confirmed'] = pd.to_numeric(data['confirmed'])
+data['deaths'] = pd.to_numeric(data['deaths'])
+
+
+#data.shape #(18834, 7)
+#data
+
+
+################
+## Export Data - Global
+################
+
+data.to_csv('covid-19-prediction/data/covid_data_global.csv', index = False)
 
