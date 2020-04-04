f1eca757055a05a091d9ff6db0d6c26bedd7bf9d
genevievelyons <glyons@hsph.harvard.edu>
2020-04-04 11:48:14 -0400

    data

---
+++ load_covid_data.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  4 10:34:08 2020
+
+@author: genevievelyons
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+import git 
+import os
+
+pd.set_option('display.max_columns', None)
+
+################
+## Pull from Git Repos
+################
+
+#pwd
+os.chdir("AC209B - Data Science II/Final Project")
+
+#Update the data
+git_dir = 'COVID-19/'
+g = git.cmd.Git(git_dir)
+g.pull()
+
+#Update the current 
+git_dir = 'covid-19-prediction/'
+g = git.cmd.Git(git_dir)
+g.pull()
+
+################
+## Load the data
+################
+
+#Load data
+confirmed = pd.read_csv('COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv', dtype = str)  
+deaths = pd.read_csv('COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv', dtype = str)  
+
+#List of keys
+keys = confirmed.loc[:,"UID":"Combined_Key"]
+
+#Columns
+c = list(keys.columns)
+c.append('confirmed')
+c.append('deaths')
+c.append('date')
+
+data = pd.DataFrame(columns = c)
+
+#Pivot the columns
+for col in confirmed.columns[11:]:
+    df = pd.concat([keys,confirmed.loc[:,col]], axis = 1)
+    df.rename(columns = {col: 'confirmed'}, inplace = True)
+    df = pd.concat([df, deaths.loc[:,col]], axis = 1)
+    df.rename(columns = {col: 'deaths'}, inplace = True)
+    df["date"] = col
+    data = pd.concat([data, df], axis = 0, sort = False)
+
+data.columns = map(str.lower, data.columns)
+
+#Data types
+data['date'] = pd.to_datetime(data['date'])
+data['lat'] = pd.to_numeric(data['lat'])
+data['long_'] = pd.to_numeric(data['long_'])
+data['confirmed'] = pd.to_numeric(data['confirmed'])
+data['deaths'] = pd.to_numeric(data['deaths'])
+
+
+
+np.sum(data[(data.combined_key.str[0:10] == 'Unassigned') & (data.date > '3/22/2020')]['confirmed'])
+np.sum(data[(data.combined_key.str[0:10] != 'Unassigned') & (data.date > '3/22/2020')]['confirmed'])
+
+51835/1641575 #3.15% of cases are unassigned
+
+#data.shape #(234216, 14)
+#data
+
+################
+## Export Data
+################
+
+data.to_csv('covid-19-prediction/data/covid_data.csv', index = False)
+
+#Push to git
+g.push()
+
